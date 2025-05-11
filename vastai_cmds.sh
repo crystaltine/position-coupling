fce7197339bc243e528025adb993e5de1b0c408b
crystaltine <msh379c@outlook.com>
2025-05-11 03:19:58 -0700

    Update vastai_cmds.sh

--- vastai_cmds.sh
+++ vastai_cmds.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/crystaltine/position-coupling/tree/main
+git clone https://github.com/crystaltine/position-coupling.git
 
 curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh; chmod +x; miniconda.sh; sh miniconda.sh -b -p /content/miniconda
 
