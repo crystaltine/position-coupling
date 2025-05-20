97c447c8068b34727c51b029cc3c8556f2f9fb7b
crystaltine <msh379c@outlook.com>
2025-05-20 02:20:12 -0700

    fix chmod

--- vastai_cmds.bash
+++ vastai_cmds.bash
@@ -1,7 +1,7 @@
 git clone https://github.com/crystaltine/position-coupling.git
 
 curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
-chmod +x; 
+chmod +x miniconda.sh; 
 miniconda.sh
 sh miniconda.sh -b -p /content/miniconda
 
