1ff60dffa94a1402dbd46825a3d7e867dedc4f59
crystaltine <msh379c@outlook.com>
2025-06-21 21:28:47 -0700

    use abspaths for setup script

--- vastai_cmds.bash
+++ vastai_cmds.bash
@@ -1,12 +1,10 @@
 git clone https://github.com/crystaltine/position-coupling.git
 
-curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
-chmod +x miniconda.sh
-sh miniconda.sh -b -p /content/miniconda
+curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh; chmod +x; miniconda.sh; sh miniconda.sh -b -p /content/miniconda
 
 export PATH="/content/miniconda/bin:$PATH"
 
-cd ./position-coupling/
+cd /workspace/position-coupling
 
 conda env create -f env.yaml
 
@@ -14,6 +12,6 @@ conda init
 
 exec bash
 
-cd position-coupling
+cd /workspace/position-coupling
 
 conda activate NLP
